175cbcae195634d2704bec28cebafba25932e035
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2022-09-25 12:25:45 -0700

    Set device class for battery state of energy

--- custom_components/solaredge_modbus_multi/sensor.py
+++ custom_components/solaredge_modbus_multi/sensor.py
@@ -1854,6 +1854,7 @@ class SolarEdgeBatterySOH(SolarEdgeSensorBase):
 
 
 class SolarEdgeBatterySOE(SolarEdgeSensorBase):
+    device_class = SensorDeviceClass.BATTERY
     state_class = SensorStateClass.MEASUREMENT
     native_unit_of_measurement = PERCENTAGE
     entity_category = EntityCategory.DIAGNOSTIC
