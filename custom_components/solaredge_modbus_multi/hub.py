12fd4a43a508653fc636236608f3392a0ae46043
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2022-09-29 12:15:04 -0700

    Qualify read exceptions during setup

--- custom_components/solaredge_modbus_multi/hub.py
+++ custom_components/solaredge_modbus_multi/hub.py
@@ -279,8 +279,16 @@ class SolarEdgeModbusMultiHub:
             for battery in self.batteries:
                 await self._hass.async_add_executor_job(battery.read_modbus_data)
 
-        except Exception:
-            raise HubInitFailed("Devices not ready.")
+        except ModbusReadError as e:
+            self.online = False
+            self.disconnect()
+            raise HubInitFailed(f"Read error: {e}")
+
+        except DeviceInvalid as e:
+            self.online = False
+            if not self.keep_modbus_open:
+                self.disconnect()
+            raise HubInitFailed(f"Invalid device: {e}")
 
         self.initalized = True
 
