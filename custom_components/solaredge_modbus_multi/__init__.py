804dab55bc1705beccf919f077b4e297f43a1341
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2022-11-03 11:41:39 -0700

    Advanced Power Control binary sensor

--- custom_components/solaredge_modbus_multi/__init__.py
+++ custom_components/solaredge_modbus_multi/__init__.py
@@ -40,8 +40,7 @@ from .hub import DataUpdateFailed, HubInitFailed, SolarEdgeModbusMultiHub
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[str] = [Platform.NUMBER, Platform.SELECT, Platform.SENSOR]
-
+PLATFORMS: list[str] = [Platform.BINARY_SENSOR,, Platform.NUMBER, Platform.SELECT, Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up SolarEdge Modbus from a config entry."""
