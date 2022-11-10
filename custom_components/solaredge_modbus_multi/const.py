1efae649b5e4593dde182953a4cd8076d79abed2
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2022-11-10 11:22:05 -0800

    Update meter related names

--- custom_components/solaredge_modbus_multi/const.py
+++ custom_components/solaredge_modbus_multi/const.py
@@ -252,8 +252,8 @@ STOREDGE_MODE = {
 
 LIMIT_CONTROL_MODE = {
     None: "Disabled",
-    0: "Export/Import Meter",
-    1: "Consumption Meter",
+    0: "Using Export/Import Meter",
+    1: "Using Consumption Meter",
     2: "Production Limitation",
 }
 
