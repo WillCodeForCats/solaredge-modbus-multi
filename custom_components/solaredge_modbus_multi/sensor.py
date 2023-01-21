894beaea9da627a3f837e494819e14141fd784c3
Seth <48533968+WillCodeForCats@users.noreply.github.com>
2023-01-21 09:15:52 -0800

    Order imports with isort

--- custom_components/solaredge_modbus_multi/sensor.py
+++ custom_components/solaredge_modbus_multi/sensor.py
@@ -24,8 +24,8 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
-    BATTERY_STATUS,
     BATTERY_RATED_ADJUSTMENT,
+    BATTERY_STATUS,
     DEVICE_STATUS,
     DEVICE_STATUS_DESC,
     DOMAIN,
