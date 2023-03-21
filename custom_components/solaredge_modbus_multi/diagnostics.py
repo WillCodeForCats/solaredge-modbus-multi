9962c3d6161fddc1d15c20e94d5eeaa34a309112
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2023-03-21 11:20:20 -0700

    Update diagnostics.py

--- custom_components/solaredge_modbus_multi/diagnostics.py
+++ custom_components/solaredge_modbus_multi/diagnostics.py
@@ -7,18 +7,16 @@ from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from . import DOMAIN, SolarEdgeCoordinator
-
-TO_REDACT = {}
+REDACT_CONFIG = {}
 
 
 async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, config_entry: ConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
-    coordinator: SolarEdgeCoordinator = hass.data[DOMAIN][config_entry.entry_id]
 
-    return {
-        "config_entry": async_redact_data(config_entry.as_dict(), TO_REDACT),
-        "data": coordinator.data,
+    data: dict[str, Any] = {
+        "config_entry": async_redact_data(config_entry.as_dict(), REDACT_CONFIG)
     }
+
+    return data
