1ef3f628641a8393a25ba21d3f7110924ccf8ed8
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2023-03-22 08:42:08 -0700

    Add devices to diagnostic output

--- custom_components/solaredge_modbus_multi/diagnostics.py
+++ custom_components/solaredge_modbus_multi/diagnostics.py
@@ -7,6 +7,8 @@ from homeassistant.components.diagnostics import async_redact_data
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
+
 REDACT_CONFIG = {}
 
 
@@ -14,9 +16,41 @@ async def async_get_config_entry_diagnostics(
     hass: HomeAssistant, config_entry: ConfigEntry
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
+    hub = hass.data[DOMAIN][config_entry.entry_id]["hub"]
 
     data: dict[str, Any] = {
         "config_entry": async_redact_data(config_entry.as_dict(), REDACT_CONFIG)
     }
+
+    for inverter in hub.inverters:
+        inverter: dict[str, Any] = {
+            f"inverter_unit_id_{inverter.inverter_unit_id}": {
+                "common": inverter.decoded_common,
+                "model": inverter.decoded_model,
+                "mmppt": inverter.decoded_mmppt,
+                "storage": inverter.decoded_storage,
+            }
+        }
+        data.update(inverter)
+
+    for meter in hub.meters:
+        meter: dict[str, Any] = {
+            f"meter_id_{meter.meter_id}": {
+                "inverter_unit_id": meter.inverter_unit_id,
+                "common": meter.decoded_common,
+                "model": meter.decoded_model,
+            }
+        }
+        data.update(meter)
+
+    for battery in hub.batteries:
+        battery: dict[str, Any] = {
+            f"battery_id_{battery.battery_id}": {
+                "inverter_unit_id": battery.inverter_unit_id,
+                "common": battery.decoded_common,
+                "model": battery.decoded_model,
+            }
+        }
+        data.update(battery)
 
     return data
