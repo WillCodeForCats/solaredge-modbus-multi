c147e349b0ea8cf7aee2849523b3b7dcf388567a
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2023-04-21 15:09:35 -0700

    Increase starting wait to 800 ms

--- custom_components/solaredge_modbus_multi/__init__.py
+++ custom_components/solaredge_modbus_multi/__init__.py
@@ -186,7 +186,7 @@ class SolarEdgeCoordinator(DataUpdateCoordinator):
                 return await self._refresh_modbus_data_with_retry(
                     ex_type=DataUpdateFailed,
                     limit=4,
-                    wait_ms=400,
+                    wait_ms=800,
                 )
 
         except HubInitFailed as e:
