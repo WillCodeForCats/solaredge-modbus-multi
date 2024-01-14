d0b0e77787cad8c40c7e86b1907c57c9ebf54392
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2024-01-14 07:17:58 -0800

    Revert "Remove scale_factor helper function"
    This reverts commit 4b7a8e6a42fda57a59796ff938184a6827f2e95a.

--- custom_components/solaredge_modbus_multi/helpers.py
+++ custom_components/solaredge_modbus_multi/helpers.py
@@ -6,6 +6,13 @@ import struct
 from .const import DOMAIN_REGEX
 
 
+def scale_factor(value: int, sf: int):
+    try:
+        return value * (10**sf)
+    except ZeroDivisionError:
+        return 0
+
+
 def float_to_hex(f):
     try:
         return hex(struct.unpack("<I", struct.pack("<f", f))[0])
