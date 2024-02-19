c5ec7f299ec2812443f8b8ed0fac9f3eb3974206
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2024-02-19 10:42:58 -0800

    Include inverter ID in meter and battery names

--- custom_components/solaredge_modbus_multi/hub.py
+++ custom_components/solaredge_modbus_multi/hub.py
@@ -1436,7 +1436,9 @@ class SolarEdgeMeter:
         self.fw_version = self.decoded_common["C_Version"]
         self.serial = self.decoded_common["C_SerialNumber"]
         self.device_address = self.decoded_common["C_Device_address"]
-        self.name = f"{self.hub.hub_id.capitalize()} M{self.meter_id}"
+        self.name = (
+            f"{self.hub.hub_id.capitalize()} I{self.inverter_unit_id} M{self.meter_id}"
+        )
 
         inverter_model = self.inverter_common["C_Model"]
         inerter_serial = self.inverter_common["C_SerialNumber"]
@@ -1686,7 +1688,7 @@ class SolarEdgeBattery:
         self.fw_version = self.decoded_common["B_Version"]
         self.serial = self.decoded_common["B_SerialNumber"]
         self.device_address = self.decoded_common["B_Device_Address"]
-        self.name = f"{self.hub.hub_id.capitalize()} B{self.battery_id}"
+        self.name = f"{self.hub.hub_id.capitalize()} I{self.inverter_unit_id} B{self.battery_id}"
 
         inverter_model = self.inverter_common["C_Model"]
         inerter_serial = self.inverter_common["C_SerialNumber"]
