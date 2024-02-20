65d90e570b23d2088d1d9081d87f2c346c30c951
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2024-02-20 08:54:06 -0800

    Revert "Flag base class"
    This reverts commit 8cb3852b815a088352a25894db6efc4cd07eec8b.

--- custom_components/solaredge_modbus_multi/const.py
+++ custom_components/solaredge_modbus_multi/const.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import re
-from enum import Flag, IntEnum, StrEnum
+from enum import IntEnum, StrEnum
 from typing import Final
 
 DOMAIN = "solaredge_modbus_multi"
@@ -82,7 +82,7 @@ class ConfDefaultInt(IntEnum):
     BATTERY_ENERGY_RESET_CYCLES = 0
 
 
-class ConfDefaultFlag(Flag):
+class ConfDefaultFlag(IntEnum):
     """Defaults for options that are booleans."""
 
     DETECT_METERS = 1
