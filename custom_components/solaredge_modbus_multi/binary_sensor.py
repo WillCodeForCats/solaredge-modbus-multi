34385a134d52e4d97c3090b09d879b18d8253a58
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2024-07-07 12:27:53 -0700

    Shorter name for sensor

--- custom_components/solaredge_modbus_multi/binary_sensor.py
+++ custom_components/solaredge_modbus_multi/binary_sensor.py
@@ -114,7 +114,7 @@ class GridStatusOnOff(SolarEdgeBinarySensorBase):
 
     @property
     def name(self) -> str:
-        return "Grid Power Available"
+        return "Grid Power"
 
     @property
     def entity_registry_enabled_default(self) -> bool:
