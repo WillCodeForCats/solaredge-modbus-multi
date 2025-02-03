4231d02c41ac7b56c7788ff259622361bba42823
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2025-02-02 20:00:35 -0800

    Add mb_str function

--- custom_components/solaredge_modbus_multi/solaredge.py
+++ custom_components/solaredge_modbus_multi/solaredge.py
@@ -2,3 +2,12 @@
 
 from __future__ import annotations
 
+
+class SolarEdgeModbusDevice:
+    """Helper functions for SolarEdge modbus devices."""
+
+    @staticmethod
+    def mb_str(s: bytes | str) -> str:
+        if isinstance(s, bytes):
+            s = s.decode(encoding="utf-8", errors="ignore")
+        return s.replace("\x00", "").rstrip()
