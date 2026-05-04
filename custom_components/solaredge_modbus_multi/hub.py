cd5b094736df930e69eb44113871dd41c8b19026
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2026-05-03 19:09:48 -0700

    Add read for cable state

--- custom_components/solaredge_modbus_multi/hub.py
+++ custom_components/solaredge_modbus_multi/hub.py
@@ -2805,6 +2805,24 @@ class SolarEdgeEVSE:
                 ]
             )
 
+            evse_data = await self.hub.modbus_read_holding_registers(
+                unit=self.evse_unit_id, address=1004, rcount=2
+            )
+
+            self.decoded_model.update(
+                OrderedDict(
+                    [
+                        (
+                            "E_Cable_State",
+                            ModbusClientMixin.convert_from_registers(
+                                evse_data.registers[0:2],
+                                data_type=ModbusClientMixin.DATATYPE.UINT32,
+                            ),
+                        ),
+                    ]
+                )
+            )
+
         except ModbusIllegalAddress:
             _LOGGER.error(f"E{self.evse_unit_id}: E_State NOT available")
 
