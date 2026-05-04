3974e63a2cbf271cac59461ea00db2c052756f5b
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2026-05-03 19:10:01 -0700

    Add read for type and features

--- custom_components/solaredge_modbus_multi/hub.py
+++ custom_components/solaredge_modbus_multi/hub.py
@@ -2823,6 +2823,23 @@ class SolarEdgeEVSE:
                 )
             )
 
+            evse_data = await self.hub.modbus_read_holding_registers(
+                unit=self.evse_unit_id, address=1016, rcount=2
+            )
+
+            self.decoded_model.update(
+                OrderedDict(
+                    [
+                        (
+                            "E_Type_Features",
+                            ModbusClientMixin.convert_from_registers(
+                                evse_data.registers[0:2],
+                                data_type=ModbusClientMixin.DATATYPE.UINT32,
+                            ),
+                        ),
+                    ]
+                )
+            )
         except ModbusIllegalAddress:
             _LOGGER.error(f"E{self.evse_unit_id}: E_State NOT available")
 
