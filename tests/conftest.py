e3533baabad9c5ae008afab0bf1249e3022c8051
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2026-08-18 14:24:50 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
-"""Activate pytest-homeassistant-custom-component for all tests."""
+"""Activate pytest-homeassistant-custom-component and modbus-connection for all tests."""
 
-pytest_plugins = "pytest_homeassistant_custom_component"
+pytest_plugins = [
+    "pytest_homeassistant_custom_component",
+    "modbus_connection.pytest_plugin",
+]
