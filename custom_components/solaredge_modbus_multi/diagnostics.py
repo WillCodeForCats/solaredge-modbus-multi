2fceef60cb9c293dcdf5c2af14686b65f5a00eed
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2026-09-15 18:33:18 -0700

    Fix diagnostics for multiple meter/battery/der

--- custom_components/solaredge_modbus_multi/diagnostics.py
+++ custom_components/solaredge_modbus_multi/diagnostics.py
@@ -110,7 +110,7 @@ async def async_get_config_entry_diagnostics(
 
     for meter in hub.meters:
         meter: dict[str, Any] = {
-            f"meter_id_{meter.meter_id}": {
+            f"meter_id_I{meter.inverter_unit_id}_M{meter.meter_id}": {
                 "device_info": meter.device_info,
                 "inverter_unit_id": meter.inverter_unit_id,
                 "common": component_to_dict(meter.meter_info),
@@ -121,7 +121,7 @@ async def async_get_config_entry_diagnostics(
 
     for battery in hub.batteries:
         battery: dict[str, Any] = {
-            f"battery_id_{battery.battery_id}": {
+            f"battery_id_I{battery.inverter_unit_id}_B{battery.battery_id}": {
                 "device_info": battery.device_info,
                 "inverter_unit_id": battery.inverter_unit_id,
                 "common": component_to_dict(battery.battery_info),
@@ -132,10 +132,10 @@ async def async_get_config_entry_diagnostics(
 
     for der_battery in hub.der_batteries:
         der_battery: dict[str, Any] = {
-            f"battery_id_{der_battery.battery_id}": {
+            f"der_battery_id_I{der_battery.inverter_unit_id}"
+            f"_DERB{der_battery.battery_id}": {
                 "device_info": der_battery.device_info,
                 "inverter_unit_id": der_battery.inverter_unit_id,
-                "common": component_to_dict(der_battery.der_storage_capacity_data),
                 "model": format_values(
                     component_to_dict(der_battery.der_storage_capacity_data)
                 ),
@@ -147,7 +147,6 @@ async def async_get_config_entry_diagnostics(
         evse: dict[str, Any] = {
             f"evse_unit_id_{evse.evse_unit_id}": {
                 "device_info": evse.device_info,
-                "common": component_to_dict(evse.evse_common),
                 "model": format_values(component_to_dict(evse.evse_common)),
             }
         }
